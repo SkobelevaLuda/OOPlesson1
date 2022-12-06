3f1236d0ca28db75a0310deeec94c78f4eb797dc
Людок <skobeleva.ludmila@yandex.ru>
2022-12-07 00:28:39 +0300

    Первый коммит

---
+++ src/Human.java
@@ -0,0 +1,10 @@
+public class Human {
+    int yearOfBirth;
+    String name;
+    String town;
+
+    void people() {
+        System.out.println("Привет! Меня зовут "+name+" Я из города "+town+" Я родился в "+yearOfBirth+" году. " +
+                "Будем знакомы!");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        Human maksim = new Human ();
+        maksim.yearOfBirth = 1988;
+        maksim.name= " Максим ";
+        maksim.town = " Минск ";
+        maksim.people();
 
-        System.out.println("Hello world!");
+        Human anya = new Human ();
+        anya.yearOfBirth = 1993;
+        anya.name= " Аня ";
+        anya.town = " Москва ";
+        anya.people();
+
+        Human katya = new Human ();
+        katya.yearOfBirth = 1992;
+        katya.name= " Катя ";
+        katya.town = " Калининград ";
+        katya.people();
+
+        Human artem = new Human ();
+        artem.yearOfBirth = 1995;
+        artem.name= " Артем ";
+        artem.town = " Москва ";
+        artem.people();
+
     }
 }
