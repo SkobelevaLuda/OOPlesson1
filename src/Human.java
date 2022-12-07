d9e7befa71c2b0d794b4c4382768091fbc420009
Людок <skobeleva.ludmila@yandex.ru>
2022-12-07 15:13:49 +0300

    Задание 2.1 выполнено

--- src/Human.java
+++ src/Human.java
@@ -4,6 +4,13 @@ public class Human {
     String town;
     String jobTitle;
 
+    Human(String name, int yearOfBirth, String town, String jobTitle){
+        this.name=name;
+        this.yearOfBirth=yearOfBirth;
+        this.town =town;
+        this.jobTitle=jobTitle;
+    }
+
 
     void people() {
         System.out.println("Привет! Меня зовут "+name+" Я из города "+town+" Я родился в "+yearOfBirth+" году. " +
--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        Human maksim = new Human ();
-        maksim.yearOfBirth = 1988;
-        maksim.name= " Максим ";
-        maksim.town = " Минск ";
-        maksim.jobTitle = " бренд-менеджером ";
+        Human maksim = new Human (" Максим ", 1988, "Минск "," бренд-менеджером ");
         maksim.people();
 
-        Human anya = new Human ();
-        anya.yearOfBirth = 1993;
-        anya.name= " Аня ";
-        anya.town = " Москва ";
-        anya.jobTitle = " методистом образовательных программ ";
+        Human anya = new Human ( " Аня ",1993, "Москва ",
+                "  методистом образовательных программ ");
         anya.people();
 
-        Human katya = new Human ();
-        katya.yearOfBirth = 1992;
-        katya.name= " Катя ";
-        katya.town = " Калининград ";
-        katya.jobTitle = " продакт-менеджером ";
+        Human katya = new Human (" Катя ", 1992, " Калиниград ", " продакт-менеджером ");
         katya.people();
 
-        Human artem = new Human ();
-        artem.yearOfBirth = 1995;
-        artem.name= " Артем ";
-        artem.town = " Москва ";
-        artem.jobTitle = " директором по развитию бизнеса ";
+        Human artem = new Human (" Артем ", 1995, " Москва ",
+                " директором по развитию бизнеса ");
         artem.people();
 
     }
