f49a34465be615161d62c24c3a854aba7c76161f
Людок <skobeleva.ludmila@yandex.ru>
2022-12-08 19:17:48 +0300

    ООП Урок 2,задание 1.1 выполнено

--- src/Human.java
+++ src/Human.java
@@ -1,36 +1,51 @@
 public class Human {
-    int yearOfBirth;
+    private int yearOfBirth;
     String name;
-    String town;
+    private String town;
     String jobTitle;
 
-    Human(String name, int yearOfBirth, String town, String jobTitle){
-        if (name!= null){
-            this.name=name;
-        }else {
-            this.name="Информация не указана";
-        }
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
 
-        if (yearOfBirth>0){
-            this.yearOfBirth=yearOfBirth;
-        }else {
-            this.yearOfBirth=0;
-        }
-        if (town!=null){
-            this.town =town;
-        }else {
-            this.town="Информация не указана";
-        }
-        if (jobTitle!=null){
-            this.jobTitle=jobTitle;
-        }else{
-            this.town="Информация не указана";
+    public String getTown() {
+        return town;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth > 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = 0;
         }
     }
 
+    public void setTown(String town) {
+        if (town == null || town.isBlank()) {
+            this.town = "Информация не указана";
+        } else {
+            this.town = town;
+        }
+    }
+
+    public Human(String name, int yearOfBirth, String town, String jobTitle) {
+        setYearOfBirth(yearOfBirth);
+        if (name == null || name.isBlank()) {
+            this.name = "Информация не указана";
+        } else {
+            this.name = name;
+        }
+        this.jobTitle = jobTitle;
+        if (jobTitle == null || jobTitle.isBlank()) {
+            this.jobTitle = "Информация не указана";
+        } else {
+            this.jobTitle = jobTitle;
+        }
+        setTown(town);
+    }
 
     void people() {
-        System.out.println("Привет! Меня зовут "+name+" Я из города "+town+" Я родился в "+yearOfBirth+" году. " +
-                "Я работаю на должности" +jobTitle+ ". Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + " Я из города " + town + " Я родился в " + yearOfBirth + " году. " +
+                "Я работаю на должности" + jobTitle + ". Будем знакомы!");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        Human maksim = new Human (" Максим ", 1988, "Минск "," бренд-менеджером ");
+        Human maksim = new Human(" Максим ", 1988, "Минск ", " бренд-менеджером ");
         maksim.people();
 
-        Human anya = new Human ( " Аня ",1993, "Москва ",
+        Human anya = new Human("  ", 1993, "Москва ",
                 "  методистом образовательных программ ");
         anya.people();
 
-        Human katya = new Human (" Катя ", 1992, " Калиниград ", " продакт-менеджером ");
+        Human katya = new Human(" Катя ", 1992, " Калиниград ", " продакт-менеджером ");
         katya.people();
 
-        Human artem = new Human (" Артем ", -1995, " Москва ",
+        Human artem = new Human(" Артем ", -1995, " Москва ",
                 " директором по развитию бизнеса ");
         artem.people();
+
+        Human vladimir = new Human(" Владимир ", 2001, " Казань ", " безработный ");
+        vladimir.people();
+
+        Flover roza = new Flover(" Роза", " Голландия ", " ", 35.59, 0);
+        roza.flovers();
+
+        Flover hrizantema = new Flover("Хризантема", " ", "", 15.00, -5);
+        hrizantema.flovers();
+
+        Flover pion = new Flover("Пион", " Англия ", "", 69.90, 1);
+        pion.flovers();
+
+        Flover gipsofila = new Flover("Гипсофила", " Турция ", "", 19.50, 10);
+        gipsofila.flovers();
+
 
     }
 }
